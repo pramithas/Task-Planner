1ad7e709181c98e009a410fb5aa9045a56300cbe
Pramithas Dhakal <pramithas.dhakal@renegadeinsurance.com>
2023-04-22 10:53:00 +0545

    Task planner app

---
+++ src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles.css';
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+        <input type="input" 
+        value={todo}
+        onChange = {  (e) => setTodo(e.target.value)} 
+        className="input_box" placeholder='Enter a task' />
+        <button className="input_submit" type="submit">
+            Go
+        </button>
+    </form>
+  )
+}
+
+export default InputField
---
+++ src/model.ts
@@ -0,0 +1,23 @@
+import { useReducer } from "react";
+
+export interface Todo {
+    id: number;
+    todo:string;
+    isDone:boolean;
+}
+
+type Actions = {type:'add'; payload: string} |
+               {type:'remove'; payload:number} |
+               {type:'done'; payload: number };
+
+// const TodoReducer = (state:Todo[], action) => {
+
+// }
+
+// const ReducerExample = () => {
+
+//     const [state, dispatch] = useReducer(TodoReducer, []);
+//   return (
+//     <div></div>
+//   )
+// }
